abf9fb2a0ddcf5fd3f54d16934b9623e634bb525
javi-diaz47 <javiereduardo300@gmail.com>
2022-10-11 18:11:49 -0500

    fix(offers/[id]): correct typo in profile photo href

--- pages/offers/[id].jsx
+++ pages/offers/[id].jsx
@@ -9,7 +9,7 @@ export default function Offer(offer) {
   return (
     <div className="offer">
       <div className="flex gap-4">
-        <Link href={`profile/${offer.owner_id}`}>
+        <Link href={`../profile/${offer.owner_id}`}>
           <a>
             <div className="w-12 h-12 rounded-full bg-slate-300"></div>
           </a>
