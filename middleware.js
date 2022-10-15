f46768ee30b05d77e62719e88ca11a43ee3ba163
javi-diaz47 <javiereduardo300@gmail.com>
2022-10-15 10:23:55 -0500

    feat: add middleware to handdle the authentication to access to multiple protected routes

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const token = req.cookies.get('token');
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/protected/:path*',
+};
